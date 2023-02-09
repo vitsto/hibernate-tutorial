eff05903f936f377f6c1807bb630c5fb9bf1f046
vitsto <stolyarov.vitalij@list.ru>
2023-02-10 02:12:47 +0300

    initial commit

---
+++ src/main/java/ru/stolyarov/jdbc/TestJdbc.java
@@ -0,0 +1,19 @@
+package ru.stolyarov.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+        String user = "hbstudent";
+        String password = "hbstudent";
+        try {
+            System.out.println("Connecting to database: " + jdbcUrl);
+            Connection myConn = DriverManager.getConnection(jdbcUrl,user,password);
+            System.out.println("Connection successful!");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
